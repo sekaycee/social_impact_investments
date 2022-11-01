a3092d7751fa08115f3efbe39d21bcf063a6b280
Kelechi Egbuta <se.kaycee@gmail.com>
2022-11-01 09:41:00 +0100

    refactor code

--- front/src/pages/insights/home.js
+++ front/src/pages/insights/home.js
@@ -1,6 +1,15 @@
+import Head from "next/head"
+
 export default function InsightHome() {
     return (
         <>
+            <Head>
+				<title>Insights · Social Impact Investment</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+            <section>
+                <div></div>
+            </section>
             <section className="module">
                 <div className="container">
                     <h1>Home for all our Insights</h1>
